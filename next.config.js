2c0e89156c2794c0bf7cff834aeaa4c6e5dcb8c0
monishwarmc <monishwar69@gmail.com>
2023-07-31 05:19:21 +0530

    gh-pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async generateStaticParams() {
+        return {
+          // Add your static paths here, for example:
+          '/': { page: '/' },
+          '/about': { page: '/about' },
+          // Add more paths as needed
+        };
+      },
+};
 
 module.exports = nextConfig
